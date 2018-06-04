84ad7fb8636f773af347010b9cfb377a7aa77b59
Olga Ivancic <olgap2015@users.noreply.github.com>
2018-06-04 23:33:31 +0200

    Version 1.0 finished

--- src/main/tests/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/main/tests/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -1,5 +1,6 @@
 package com.teamtreehouse.techdegree.overboard.model;
 
+import com.teamtreehouse.techdegree.overboard.exc.AnswerAcceptanceException;
 import com.teamtreehouse.techdegree.overboard.exc.VotingException;
 import org.junit.Before;
 import org.junit.Rule;
@@ -21,7 +22,7 @@ public class UserTest {
     public ExpectedException thrown = ExpectedException.none();
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         board = new Board("Java");
         questioner = board.createUser("Olga");
         answerer = board.createUser("Craig");
@@ -73,5 +74,36 @@ public class UserTest {
         thrown.expectMessage("You cannot vote for yourself!");
 
         questioner.downVote(question);
+    }
+
+    @Test
+    public void downvotingTheAnswerIsNotAllowedForAuthor() {
+        thrown.expect(VotingException.class);
+        thrown.expectMessage("You cannot vote for yourself!");
+
+        answerer.downVote(answer);
+    }
+
+    @Test
+    public void acceptingAnswerByRandomUserIsNotAllowed() {
+        thrown.expect(AnswerAcceptanceException.class);
+        thrown.expectMessage("Only Olga can accept this answer as it is their question");
+
+        reader.acceptAnswer(answer);
+    }
+
+    @Test
+    public void acceptingAnswerByAnswererIsNotAllowed() {
+        thrown.expect(AnswerAcceptanceException.class);
+        thrown.expectMessage("Only Olga can accept this answer as it is their question");
+
+        answerer.acceptAnswer(answer);
+    }
+
+    @Test
+    public void downvotingAnswerSubtractsVotesFromAuthor() {
+        reader.downVote(answer);
+
+        assertEquals(-1, answerer.getReputation());
     }
 }
